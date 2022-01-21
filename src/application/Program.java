2e162bcec0ff4c6477b78e6098844df997c2c6ce
Luiz Felipe <luiz.farias.cpv@gmail.com>
2022-01-20 21:01:49 -0300

    Implementing position class

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import boardgame.Position;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Position position = new Position(3,5);
+		
+		System.out.println(position);
+		
+	}
+
+}
