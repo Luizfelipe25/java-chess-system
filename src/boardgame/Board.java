4a959cebcf7aaa2d551b8e02955749d6a6a5ff49
Luiz Felipe <luiz.farias.cpv@gmail.com>
2022-01-24 19:22:41 -0300

    exceptions and protecting the board class

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -7,6 +7,9 @@ public class Board {
 	
 	
 	public Board(int rows, int columns) {
+		if(rows < 1 || columns < 1) {
+			throw new BoardException ("ERRO ao criar o tabuleiro: Precisa ter ao menos 1 linha e 1 coluna");
+		}
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
@@ -17,30 +20,46 @@ public class Board {
 		return rows;
 	}
 
-
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
-
 	public int getColumns() {
 		return columns;
 	}
-
-
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
 	
 	public Piece piece (int row, int column) {
+		if(!positionExists(row,column)) {
+			throw new BoardException("Posição nao esta no tabuleiro");
+		}
 		return pieces[row][column];
 	}
 	
 	public Piece Piece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Posição nao esta no tabuleiro");
+		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
 	
+	public void placePiece(Piece piece, Position position ) {
+		if(thereIsAPiece(position)) {
+			throw new BoardException("Já existe uma peça nessa posiçao "+ position );
+		}
+		pieces[position.getRow()][position.getColumn()] = piece;
+		piece.position = position;
+		
+	}
 	
+	private boolean positionExists(int row, int column) {
+		return row  >= 0 && row < rows && column >= 0 && column < columns;
+	}
 	
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+	}
 	
+	public boolean thereIsAPiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Posição nao esta no tabuleiro");
+		}
+		return Piece(position) != null;
+		
+	}
 }
--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -6,7 +6,7 @@ import boardgame.Piece;
 public class ChessPiece extends Piece {
 	private Color color;
 	
-	public ChessPiece(Board board) {
+	public ChessPiece(Board board, Color color) {
 		super(board);
 		this.color = color;
 	}
