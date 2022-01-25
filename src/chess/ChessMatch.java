5bb9e51d57be5c9c2a00cfb601b707864c62aecf
Luiz Felipe <luiz.farias.cpv@gmail.com>
2022-01-24 21:14:45 -0300

    rook possible moves

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -29,6 +29,7 @@ public class ChessMatch {
 		Position source =  sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
 		validateSourcePosition(source);
+		validateTargetPosition(source,target);
 		Piece capturedPiece = makeMove(source, target);
 		return (ChessPiece)capturedPiece;
 	}
@@ -48,6 +49,12 @@ public class ChessMatch {
 			throw new ChessException("Não existe movimentos possiveis para a peça escolhida");
 		}
 		
+	}
+	
+	private void validateTargetPosition(Position source, Position target) {
+		if(!board.piece(source).possibleMove(target)) {
+			throw new ChessException("A peça escolhida nao pode se mover para a posiçao " + target);
+		}
 	}
 	
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
